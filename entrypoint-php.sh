24dae8abe34c82e55114f472f016e9cfebb8b7c7
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-04-20 15:09:01 +0200

    handle stop signals

--- entrypoint-php.sh
+++ entrypoint-php.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function exit_container_SIGTERM(){
+  echo "Caught SIGTERM"
+  exit 0
+}
+trap exit_container_SIGTERM SIGTERM
+
 echo "Setting /app/public ownership..."
 chgrp -R 33 /app/public
 chown -hR 33:33 /app/public
@@ -8,4 +14,4 @@ echo "Setting permissions for /app/public/var..."
 chmod a+w /app/public/var/*
 
 echo "Starting PHP-FPM..."
-php-fpm
+php-fpm -F & wait

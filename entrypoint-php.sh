e5ae7b2364202ce2165f022a68f863d91c453884
br0kenpixel <br0kenpixel@varganet.org>
2026-07-08 13:17:44 +0200

    improve entrypoint script
    - exit on error
    - only apply perms fixup for `/app/public/var` if it exists
    - automatically download Shopware installer if `/app/public` is empty

--- entrypoint-php.sh
+++ entrypoint-php.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 function exit_container_SIGTERM(){
   echo "Caught SIGTERM"
   exit 0
@@ -10,8 +12,19 @@ echo "Setting /app/public ownership..."
 chgrp -R 33 /app/public
 chown -hR 33:33 /app/public
 
-echo "Setting permissions for /app/public/var..."
-chmod a+w /app/public/var/*
+if [ -d "/app/public/var" ]; then
+    echo "Directory /app/public/var exists, setting permissions..."
+    chmod a+w /app/public/var/*
+else
+    echo "Directory /app/public/var does not exist"
+fi
+
+if [ -z "$(find /app/public -mindepth 1 -maxdepth 1)" ]; then
+  echo "Application directory is empty, downloading Shopware installer..."
+  wget https://github.com/shopware/web-recovery/releases/latest/download/shopware-installer.phar.php -O /app/public/shopware-installer.phar.php --no-verbose
+else
+  echo "Application directory is not empty, not downloading Shopware installer"
+fi
 
 echo "Starting PHP-FPM..."
 php-fpm -F & wait
